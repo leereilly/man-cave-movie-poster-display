d8e088cf9be116bcb338322859b4c7f1a6e5d78c
Lee Reilly <lee@github.com>
2020-01-12 16:16:15 -0800

    Ignore DVDs

--- poster.rb
+++ poster.rb
@@ -1,6 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
 
+def download_image(url, dest)
+  open(url) do |u|
+    File.open(dest, 'wb') { |f| f.write(u.read) }
+  end
+end
+
 posters  = Array.new
 url     = 'https://www.joblo.com/movie-posters'
 doc     = Nokogiri::HTML(open(url))
@@ -14,5 +20,13 @@ posters = doc.css('img').map{ |i| i['src'] }
 # https://www.joblo.com/assets/images/joblo/posters/2020/01/birdspreyhyenaman.jpg
 
 posters.each do |poster|
-  puts 'https://www.joblo.com/' + poster.gsub("_thumb", "") if poster.include? 'thumb'
+  if (poster.include? 'thumb') && (poster.include? 'poster')
+    image_url = 'https://www.joblo.com/' + poster.gsub("_thumb", "")
+    filename = "posters/" + File.basename(image_url)
+
+    open(filename, 'wb') do |file|
+        puts "Writing #{image_url} to #{filename}"
+        file << open(image_url).read
+      end
+  end
 end
